0c0aac585fa18c98e305897bd5281bb29e76bb51
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-22 21:04:33 -0300

    feat: implemented In memory client repository

---
+++ test/repositories/in-memory-client.ts
@@ -0,0 +1,10 @@
+import { Client } from '../../src/application/entities/client';
+import { IClientRepository } from '../../src/application/repositories/client-repository';
+
+export class InMemoryClientRepository implements IClientRepository {
+  public clients: Client[] = [];
+
+  async create(data: Client): Promise<void> {
+    this.clients.push(data);
+  }
+}
