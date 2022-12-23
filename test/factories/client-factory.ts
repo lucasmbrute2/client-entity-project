d10ed0e539ad7a6424b162b54cf60266d211e566
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-23 20:35:04 -0300

    feat: Implemented client factory

---
+++ test/factories/client-factory.ts
@@ -0,0 +1,15 @@
+import { ClientProps } from 'src/application/entities/abstract-client';
+import { Client } from 'src/application/entities/client';
+
+type Override = Partial<ClientProps>;
+
+export function makeClient(override?: Override): Client {
+  return new Client({
+    firstName: 'Lucas',
+    lastName: 'Dantas',
+    password: 'Adkad!321321',
+    phone: '31975464383',
+    username: 'lucasmbrute2',
+    ...override,
+  });
+}
