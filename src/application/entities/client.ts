2f4a4a2ba504d3f25e10b5a4ffd4d0e55f3a610e
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-23 05:16:59 -0300

    feat: adding a interface props

--- src/application/entities/client.ts
+++ src/application/entities/client.ts
@@ -2,11 +2,21 @@ import { randomUUID } from 'node:crypto';
 import { Replace } from 'src/helpers/Replace';
 import { ClientProps } from './abstract-client';
 
+interface props {
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  createdAt?: Date;
+  modifiedAt?: Date | null;
+}
+
 export class Client extends ClientProps {
   private _id: string;
-  private props: ClientProps;
+  private props: props;
 
-  constructor(props: Replace<ClientProps, { createdAt?: Date }>, id?: string) {
+  constructor(props: Replace<props, { createdAt?: Date }>, id?: string) {
     super();
     this._id = id ?? randomUUID();
     this.props = {
