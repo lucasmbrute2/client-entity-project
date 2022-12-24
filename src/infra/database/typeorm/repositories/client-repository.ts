68af564785ffa586f5bfd08f9607d7b5168ad42d
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-24 16:58:55 -0300

    feat: Implemented client repository

---
+++ src/infra/database/typeorm/repositories/client-repository.ts
@@ -0,0 +1,28 @@
+import { Client } from 'src/application/entities/client';
+import { IClientRepository } from 'src/application/repositories/client-repository';
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../data-source';
+
+export class ClientRepository implements IClientRepository {
+  public repository: Repository<Client>;
+  constructor() {
+    this.repository = AppDataSource.getRepository(Client);
+  }
+
+  async create(data: Client): Promise<void> {
+    const user = this.repository.create(data);
+    await this.repository.save(user);
+  }
+
+  async findByID(id: string): Promise<Client> {
+    return await this.repository.findOneBy({
+      id,
+    });
+  }
+
+  async findByUsername(username: string): Promise<Client> {
+    return await this.repository.findOneBy({
+      username,
+    });
+  }
+}
