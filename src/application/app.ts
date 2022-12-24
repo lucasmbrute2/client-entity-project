6258fe4d4e9d477f807654517f908fd6c59f2770
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-24 17:10:48 -0300

    feat: implemented route

--- src/application/app.ts
+++ src/application/app.ts
@@ -3,7 +3,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { createConnection } from '../infra/database/typeorm/data-source';
+import { router } from 'src/infra/http/routes';
+
 const app = express();
+app.use(express.json());
+app.use(router);
 createConnection();
 
 app.get('/', (req, res) => {
