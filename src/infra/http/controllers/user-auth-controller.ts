37022b86636a93841d4f10e7b4bc895116f7a2e1
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-24 17:10:32 -0300

    feat: implemented auth client controller

---
+++ src/infra/http/controllers/user-auth-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { AuthClientUseCase } from 'src/application/use-cases/client/auth-client';
+import { container } from 'tsyringe';
+
+export class ClientAuthController {
+  async handle(req: Request, res: Response) {
+    const userAuthUseCase = container.resolve(AuthClientUseCase);
+    const { username, password } = req.body;
+
+    const { token } = await userAuthUseCase.execute({
+      username,
+      password,
+    });
+
+    //TODO VIEW MAPPER
+    return res.send(token);
+  }
+}
